22efa6cbab38fc745f58706e4fbf9e13a38200a6
Nicholas Chai <nicholas.chai@outlook.com>
2024-01-20 09:55:38 +0800

    added dis and server.js for beanstalk

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Serve static files from the React app
+// Serve static files from the React app build directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// The "catchall" handler
+// The "catchall" handler: for any request that doesn't
+// match one above, send back the React index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
